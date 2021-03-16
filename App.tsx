ab3a8bd83b8155b4cb55eff551d970231fdebc6a
madhuy <madhuy@gendevs.com>
2021-03-16 15:47:27 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems:'center'}}>
+      <Text>{`Hello world`}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
