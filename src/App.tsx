a7c3558671863568dc71b905bfa0ef1607ce54f8
madhuy <madhuy@gendevs.com>
2021-03-16 17:43:40 +0530

    file  configurations

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    // SafeAreaView,
+    // StyleSheet,
+    // ScrollView,
+    View,
+    Text,
+    // StatusBar,
+} from 'react-native';
+const App = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'red' }}>
+            <Text>Hello world</Text>
+        </View>
+    );
+};
+// const styles = StyleSheet.create({});
+export default App;
